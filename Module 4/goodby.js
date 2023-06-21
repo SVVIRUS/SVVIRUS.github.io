b0edef7262c15d0a27a97b1719b264f45f0debff
VISHAL YADAV <93426434+SVVIRUS@users.noreply.github.com>
2023-06-21 19:26:20 +0530

    Delete Module 4 directory

--- Module 4/goodby.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-	var speakWord = "GOOD BYE EVERYBODY";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	window.byeSpeaker = byeSpeaker;
-})(window);
